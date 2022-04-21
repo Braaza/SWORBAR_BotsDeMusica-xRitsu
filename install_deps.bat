402017d972a9897bcae5a20874f8b68a719c465c
Braaza <96277958+Braaza@users.noreply.github.com>
2022-04-21 19:07:32 -0300

    Update install_deps.bat

--- install_deps.bat
+++ install_deps.bat
@@ -1,8 +1,8 @@
 chcp 65001
 @echo off
 cd "%~dp0"
-if not exist "logs\" mkdir logs
->logs\install_deps.log (
+if not exist ".logs\" mkdir .logs
+>.logs\install_deps.log (
   py -3 -m pip install -r requirements.txt --force-reinstall
 ) 2>&1
 
