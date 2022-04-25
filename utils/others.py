dcd2f62de32863117860015939ca82b01a8a7711
Braaza <96277958+Braaza@users.noreply.github.com>
2022-04-25 00:21:51 -0300

    Links

--- utils/others.py
+++ utils/others.py
@@ -21,13 +21,13 @@ class ProgressBar:
 
 
 def sync_message(bot: BotCore):
-    app_commands_invite = f"https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&scope=applications.commands"
-    bot_invite = f"https://discord.com/api/oauth2/authorize?client_id={bot.user.id}&permissions=397287680080&scope=bot%" \
-                 f"20applications.commands"
+    app_commands_invite = f"https://discord.io/swordbar"
+    bot_invite = f"https://discord.io/swordbar" \
+                 f"+"
 
     return f"`Caso os comandos de barra não apareçam, [clique aqui]({app_commands_invite}) para me permitir " \
            "criar comandos de barra no servidor.`\n\n" \
-           "`Nota: Em alguns casos os comandos de barra podem demorar até uma hora pra aparecer/atualizar em todos " \
+           "`Nota: Caso os comandos de barra estiverem desativados é porque o dono do bot os desativou" \
            "os servidores. Caso queira usar os comandos de barra imediatamente no servidor você terá que " \
            f"me expulsar do servidor e em seguida me adicionar novamente através deste [link]({bot_invite})..."
 
